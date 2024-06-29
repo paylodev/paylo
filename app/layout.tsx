8376d2e2d08ef7972d23e141c06463148e79f969
Yaniv Douieb <yanivdouieb@icloud.com>
2024-06-29 23:57:38 +0200

    Add RootLayout component to app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Paylo | Solution de facturation pour les garderies scolaires",
+  description:
+    "Gérez facilement les factures de votre garderie avec Paylo, la solution de facturation dédiée aux établissements scolaires",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
