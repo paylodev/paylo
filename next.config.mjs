14d3d67102c0140ed6a7fb5b7c7656b79f7273b5
Yaniv Douieb <yanivdouieb@icloud.com>
2024-06-30 00:13:27 +0200

    Update next.config.mjs with next-intl plugin and withLogtail integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 import { withLogtail } from "@logtail/next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withLogtail(nextConfig);
+export default withLogtail(withNextIntl(nextConfig));
