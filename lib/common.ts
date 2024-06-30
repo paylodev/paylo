5b07bf4fe7d775290e4804b1dca05c122206d168
Yaniv Douieb <yanivdouieb@icloud.com>
2024-06-30 17:11:01 +0200

    Fix typo in passwordRegex pattern

--- lib/common.ts
+++ lib/common.ts
@@ -1,6 +1,6 @@
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const passwordRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`\\\-]).{8,}$/;
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;"'<>,.?/~`\\-]).{8,}$/;
 export function getMissingFields(fields: Partial<{ [key: string]: any }>) {
   return Object.entries(fields)
     .filter(([, value]) => !value)
