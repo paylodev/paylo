f74a88bb0d0a42fdb8c42a7fef13b16893b05d69
Yaniv Douieb <yanivdouieb@icloud.com>
2024-06-30 19:10:02 +0200

    Add transporter module

---
+++ lib/transporter.ts
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string),
+  secure: process.env.SMTP_SECURE === "true",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export { transporter };
