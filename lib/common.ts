2e5417f62fac341830f1d40f509efe6230e29a11
Yaniv Douieb <yanivdouieb@icloud.com>
2024-06-30 17:03:52 +0200

    Add getMissingFields function for retrieving missing fields in an object

--- lib/common.ts
+++ lib/common.ts
@@ -1,3 +1,8 @@
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`\\\-]).{8,}$/;
+export function getMissingFields(fields: Partial<{ [key: string]: any }>) {
+  return Object.entries(fields)
+    .filter(([, value]) => !value)
+    .map(([key]) => key);
+}
