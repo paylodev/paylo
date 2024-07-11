1adc0ac4d2e5e1157752015b652e30b30e878b51
Yaniv Douieb <yanivdouieb@icloud.com>
2024-07-11 13:03:17 +0200

    Add French metadata for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,33 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import { getLocale, getMessages, getTranslations } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Paylo | Solution de facturation pour les garderies scolaires",
-  description:
-    "Gérez facilement les factures de votre garderie avec Paylo, la solution de facturation dédiée aux établissements scolaires",
-};
+export async function generateMetadata() {
+  const t = await getTranslations("client.root.layout.metadata");
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const lang = await getLocale();
+  const locales = await getMessages();
+
   return (
-    <html lang="fr">
-      <body className={inter.className}>{children}</body>
+    <html lang={lang}>
+      <body className={inter.className}>
+        <NextIntlClientProvider messages={locales}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
